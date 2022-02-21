2b476143e380381a57374f6a433171369cdf432d
melekgh <melekghrab50@gmail.com>
2022-02-21 10:47:15 +0100

    first

---
+++ abeile.js
@@ -0,0 +1,21 @@
+console.log(!!NaN == !!undefined) 
+function addition(number1,number2,number3) {
+    return number1 + number2 + number3 ;
+    
+}
+var a =5;
+var b=10;
+var c=20;
+var result  = addition(a,b,c) ;
+console.log(result);
+//   var age = prompt ("quelle age avez vous");
+//   alert ("vous avez " + age + " ans");
+  var nbr = prompt("first number");
+    var nbr1 = prompt("seconde number");
+    var result = parseFloat (nbr) + parseFloat(nbr1);
+    // alert("la somme de ces deux nombres est " + result );
+       
+     let btn = document.getElementById("myBtn");
+console.log(btn)
+     btn.removeAttribute("class");
+console.log(btn);
